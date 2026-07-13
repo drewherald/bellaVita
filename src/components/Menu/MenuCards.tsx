23dbf2582ecec2995fe7ce5a5580b4357616f5c5
drewherald <drewherald9@gmail.com>
2026-07-13 11:14:10 -0400

    added drink & brunch menus

--- src/components/Menu/MenuCards.tsx
+++ src/components/Menu/MenuCards.tsx
@@ -2,6 +2,8 @@ import { Box } from "@mui/material";
 import "../../assets/styles/Menu/MenuCards.css";
 import Menu1 from '../../assets/photos/menu/_Bella Vita Menu Designs-1.jpg'
 import Menu2 from '../../assets/photos/menu/_Bella Vita Menu Designs-2.jpg'
+import Menu3 from '../../assets/photos/menu/_Bella Vita Brunch Menu (1).jpg'
+import Menu4 from '../../assets/photos/menu/Bella Vita Cocktail, Beer & Dessert Menus-1.jpg'
 
 
 export default function MenuCards() {
@@ -14,6 +16,15 @@ export default function MenuCards() {
 
       <Box className="menuCard">
         <img src={Menu2} alt='menu' style={{maxWidth: '95%'}}/>
+      </Box>
+      
+
+        <Box className="menuCard">
+        <img src={Menu3} alt='menu' style={{maxWidth: '95%'}}/>
+      </Box>
+
+      <Box className="menuCard">
+        <img src={Menu4} alt='menu' style={{maxWidth: '95%'}}/>
       </Box>
 
     </Box>
