5c92684015d88315648ac541f1240ceb7ea878e3
drewherald <drewherald9@gmail.com>
2026-09-18 09:17:55 -0400

    removed brunch menu

--- src/components/Menu/MenuCards.tsx
+++ src/components/Menu/MenuCards.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import "../../assets/styles/Menu/MenuCards.css";
 import Menu1 from '../../assets/photos/menu/_Bella Vita Menu Designs-1.jpg'
 import Menu2 from '../../assets/photos/menu/_Bella Vita Menu Designs-2.jpg'
-import Menu3 from '../../assets/photos/menu/_Bella Vita Brunch Menu (1).jpg'
+//import Menu3 from '../../assets/photos/menu/_Bella Vita Brunch Menu (1).jpg'
 import Menu4 from '../../assets/photos/menu/Bella Vita Cocktail, Beer & Dessert Menus-1.jpg'
 
 
@@ -19,9 +19,9 @@ export default function MenuCards() {
       </Box>
       
 
-        <Box className="menuCard">
+      { /* <Box className="menuCard">
         <img src={Menu3} alt='menu' style={{maxWidth: '95%'}}/>
-      </Box>
+      </Box>*/}
 
       <Box className="menuCard">
         <img src={Menu4} alt='menu' style={{maxWidth: '95%'}}/>
